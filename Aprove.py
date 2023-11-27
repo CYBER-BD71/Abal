4490e671d3738a4218144f5b0595aefe47471a81
A1F4N BRO <150107416+CYBER-BD71@users.noreply.github.com>
2023-11-27 11:26:40 +0600

    Update Aprove.py

--- Aprove.py
+++ Aprove.py
@@ -209,8 +209,7 @@ def mumit2(uid,pwx,tl):
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',
-}
+    'viewport-width': '980',}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=101',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
